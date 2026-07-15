50641a10e16d38d41b3888210f46451138a6db73
jasonyuan886 <1758396517@qq.com>
2026-07-16 01:38:44 +0800

    fix: repair sitemap.xml route.ts - replace raw XML with Next.js Route Handler

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,4 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://freshlocksealer.com/</loc>
@@ -50,4 +51,11 @@
     <changefreq>monthly</changefreq>
     <priority>0.6</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
